d22c241cd5548c15a1ac4d6ed3b1da1581ec9386
nikitawilms <nikdotde@gmail.com>
2023-09-14 08:27:46 +0200

    add project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='side project / trading algorithm using ML / strategy: momentum',
+    author='Nikita Wilms',
+    license='MIT',
+)
